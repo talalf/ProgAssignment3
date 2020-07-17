a14975508bb808944db5407027099085c63928c5
talalf <talal.fazmin1@gmail.com>
2020-07-17 13:11:55 +0100

    Question 3 function - managed to get ranking algorithm working by converting mortality columns into numeric type from character type.

---
+++ rankhospital.R
@@ -0,0 +1,38 @@
+library(dplyr)
+
+rankhospital <- function(state, outcome, num) {
+  ## Read outcome data
+  db_big <- read.csv("data/outcome-of-care-measures.csv", 
+                     colClasses = "character")
+  db_small <- select(db_big, 2, 7, 11, 17, 23)
+  
+  db <- db_small %>% rename("MI" = Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack, 
+               "HF" = Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure, 
+               "PN" = Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia)
+  
+  db$MI <- as.numeric(db$MI)
+  db$HF <- as.numeric(db$HF)
+  db$PN <- as.numeric(db$PN)
+  
+  ## Check that state and outcome are valid
+  check <- state == db$State
+  if(sum(check > 0)) {print("valid state")}
+    else {stop(print("invalid state"))}
+  
+  conditions <- c("MI", "HF", "PN")
+  check2 <- outcome == conditions
+  if(sum(check2) > 0) {print("valid condition")}
+    else {stop(print("invalid condition"))}
+  
+  ## Return hospital name in that state with the given rank
+  
+  x <- filter(db, State == state)
+  
+  if(outcome == "MI"){x <- arrange(x, x$MI, x$Hospital.Name)}
+  else if(outcome == "HF"){x <- arrange(x, x$HF, x$Hospital.Name)}
+  else if(outcome == "PN"){x <- arrange(x, x$PN, x$Hospital.Name)}
+  else{stop(print("invalid condition"))}
+  
+  paste(x[num,1], "Rank:", num, sep = " ")
+  
+}
